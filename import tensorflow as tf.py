a1a5f28e254832ac09b4b5e2279b7b41cc5e8d27
ceasoto <ceasoto@gmail.com>
2024-11-13 00:26:44 -0300

    Clase 1

---
+++ import tensorflow as tf.py
@@ -0,0 +1,10 @@
+import math
+import pandas as pd
+from keras import models, layers, optimizers, regularizers
+import numpy as np
+import random
+from sklearn import model_selection, preprocessing
+import tensorflow as tf
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
